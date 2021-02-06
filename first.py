3f4d5c4e880f58df90d10eba5237d2e2a4cfdc21
rahulsomani26 <r.somani.26@gmail.com>
2021-02-06 18:58:43 +0530

    Add files via upload
    First Python session
    comments
    variables, strings, data type, string operations

---
+++ first.py
@@ -0,0 +1,57 @@
+# This is a single line comment 
+
+'''
+ Multiline comment 
+ can span accross multiple lines 
+  
+'''
+
+"""
+    This is also a multiline comment 
+    
+"""
+
+# num1 = 10    
+# num2 = 2 
+
+# sum = num1 + num2   
+# # print(sum)
+# minus = num1 - num2 
+# mult = num1 *  num2 
+# divi = num1/num2   # division operator will always give u a floating point result ( Float data type )
+
+# ans = num1//num2  # Floor Division operator 
+
+# print(sum,minus,mult,divi,ans)
+
+# remainder = 5 % 2    # 5/2   remainder is 1 
+# print(f' The remainder of dividing {5} by {2} is {remainder}')   
+
+
+'''
+ The f-string 
+ 
+ prefix anything enclosed within '' or " " with f 
+  use {} placeholders to hold values 
+  within {} u supply the variable name  
+'''
+
+
+# name = 'Amin'
+# age = 24
+# print(f'Hello {name} your age is {age}')
+
+print(5**3)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ second.py
@@ -0,0 +1,53 @@
+'''
+ The String data type 
+ 
+ What are strings ?
+ 
+ Anything enclosed within '' or " "
+ 
+'''
+ 
+
+fname = 'rahul'
+lname = "somani"
+
+
+# Concatenation ( adding two or more strings )
+
+# print(fname + lname)
+# print(fname +  " " + lname)        
+# print(fname +'     ' + lname)
+
+# print(type("23"))
+# print(type(21.3))
+
+'''
+    Some string operations 
+    
+    1. Indexing   +ve / -ve 
+    2. Slicing    
+    
+'''    
+msg = 'Great' 
+# print(len(msg))  # len() function returns the length of an object
+
+# print(msg[0]+'2')
+# print(msg[1])
+# print(msg[2])
+# print(msg[3])
+
+'''
+ Getting multiple values from a string we use slicing 
+ syntax:
+ 
+     str[start:end]
+     str[start:end:step]
+'''
+
+# print(msg[0:3])
+# print(msg[:3])
+# print(msg[1:4])
+print(msg[:])
+
+
+
