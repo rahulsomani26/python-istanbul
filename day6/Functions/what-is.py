4c047483db3b9ce8b1db2fe0a19efd468f1ec7be
rahulsomani26 <r.somani.26@gmail.com>
2021-02-18 18:32:42 +0530

    Add files via upload

---
+++ day6/Functions/what-is.py
@@ -0,0 +1,60 @@
+'''
+   What are Functions ?                                                  25 ---625 
+                                                                         35      12 25
+                                                                         45  2025
+                                                                         55
+
+   How do we make a function ?                                           85   -  7225
+   Why at all do we need to make functions ?
+ 
+
+Syntax :
+              def function_name():
+                  set of instructions 
+
+              def coffiemachine(coffee,water,sugar,milk):
+                  set of codes 
+               
+'''
+
+# def msg(name):
+#     print(f'Hello {name}. How are you ?')
+
+# msg("rahul")
+
+
+'''
+   Write a function that sums the list 
+
+'''
+
+
+# def sum_of_list(dummy):
+#     sum = 0
+#     for val in dummy:
+#         sum = sum + val
+#         print(f' The value in this iteration is {sum}')
+#     print(f' The final sum = {sum}')
+# dummy = [2,2,1,1,3]  # sum the numbers
+# sum_of_list(dummy)
+ 
+
+# sum_of_list([1,1,1,1])
+
+
+
+
+def sum(first,second):
+    # print(first+second)
+    return (first+second)
+
+player = sum(10,20)
+print(player)
+
+
+
+
+
+
+
+
---
+++ solv.py
@@ -0,0 +1,34 @@
+'''
+    Sum and Multiplication of numbers in a list 
+    sum = 0 
+    mult = 1
+'''
+
+# dummy = [2,2,1,1,3]  # sum the numbers
+# sum =1
+# for val in dummy:
+#     sum = sum * val
+#     print(f' The value in this iteration is {sum}')
+# print(f' The final sum = {sum}')
+
+
+'''
+   Min and max numbers in a list 
+'''
+dummy = [2,4,1,0,3,-7,-1] 
+max = 1
+for item in dummy:
+    if item < max :
+        max = item 
+        print(f' The maximum value in this iteration is {max}')
+print(f' The max value in the list is {max}')
+
+
+
+
+
+
+
+
+
+
