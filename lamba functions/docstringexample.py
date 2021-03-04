abf43662e01fa24d917a346147c7a4fd5a2b9f5b
rahulsomani26 <r.somani.26@gmail.com>
2021-03-04 18:27:11 +0530

    Add files via upload

---
+++ lamba functions/docstringexample.py
@@ -0,0 +1,12 @@
+def printName(name):
+    """
+         This function takes an input and prints 
+         the input   
+    """
+
+    print(f'Hi {name}, how are you today ?')
+
+
+printName('rahul somani')
+print(printName.__doc__)  # __  dunder
+# print(printName.__dir__)
---
+++ lamba functions/whatis.py
@@ -0,0 +1,26 @@
+'''
+   Lambda Functions / Anonymous Functions   (it does not have a name )
+These are functions , work exactly the way functions work , they are used by the keyword
+lambda 
+
+Syntax: lambda arguments:expresssion 
+
+The arguments can be of any type, can be multiple , but the expression is the one which is 
+only a single statement . There is no return statement used inside a lambda function 
+
+'''
+
+
+# def add(n1, n2):
+#     # print(n1 + n2)
+#     return n1 + n2  #
+
+
+# result = add(1, 2)
+# print(result)
+
+
+def result(n1, n2): return n1 + n2
+
+
+print(result(5, 6))
